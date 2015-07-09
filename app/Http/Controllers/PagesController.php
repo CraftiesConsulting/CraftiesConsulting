e9c4b896b023ee66b0fac223f1fa4ee8c55221d6
= <=>
2015-07-09 15:49:40 +0100

    added pagescontroller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    public function sendMail(Request $request){
+        $sender_email = $request->input('email');
+        $sender_name = $request->input('name');
+        $message = $request->input('message');
+
+        Mail::send('emails.contact', ['body', $message], function ($message) use ($sender_email, $sender_name) {
+            $message->from($sender_email, $sender_name);
+            $message->to('craftiesconsulting@gmail.com')
+                    ->cc('alabamustapha@gmail.com')
+                    ->cc('info@craftiesconsulting.com');
+            $message->replyTo($sender_email);
+        });
+    }
+
+}
--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,10 +1,1 @@
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Sent from craftiesconsulting.com</title>
-</head>
-<body>
-  {{ "sending" }}
-</body>
-</html>
+welcome to laravel
